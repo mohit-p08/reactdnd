192d9d3f4885a0e11a43651f1a0ef9a6312a46fc
Mohit Prajapati <mohitprajapati11069@gmail.com>
2021-10-11 23:29:00 +0530

    react dnd components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Board from './components/Board'
+import Card from './components/Card'
+
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <center>
+                <h1>React DnD Example</h1>
+            </center>
+            <main className="flexbox">
+
+                <Board id="board-1" className="board">
+                    <center><h3>Board 1</h3></center>
+                    <br />
+                    <Card id="card-1" className="card" draggable="true">
+                        <p>Board1: card-one</p>
+                    </Card>
+                    <Card id="card-2" className="card" draggable="true">
+                        <p>Board1: card-two</p>
+                    </Card>
+                </Board>
+
+                <Board id="board-2" className="board">
+                    <center><h3>Board 2</h3></center>
+                    <br />
+                    <Card id="card-3" className="card" draggable="true">
+                        <p>Board2: card-one</p>
+                    </Card>
+                    <Card id="card-4" className="card" draggable="true">
+                        <p>Board2: card-two</p>
+                    </Card>
+                </Board>
+            </main>
+        </div>
+    )
 }
 
 export default App;
